196747fc18de9a97c4d95ba7f8b923efa63d4cbf
Alin Grigorescu <alin.grig87@gmail.com>
2023-08-07 16:38:45 +0300

    replace build path with dist path used by vite env

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.use('/api/services', serviceRouter);
 app.use('/api/appointments', appointmentRouter);
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 app.get('*', (req, res) =>
-	res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+	res.sendFile(path.join(__dirname, '/frontend/dist/index.html'))
 );
 
 const PORT = process.env.PORT || 5500;
